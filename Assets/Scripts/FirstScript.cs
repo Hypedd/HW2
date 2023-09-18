535c47e1810231f4807ccb016900b5fd26f6e127
Hypedd <59171463+Hypedd@users.noreply.github.com>
2023-09-19 01:01:48 +0900

    copyingHW1
    HW1을 복사

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("test start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+}
